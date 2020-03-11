81a3a1025e96ee4495aa3d41de9778c23b2c41f6
wangjie <17054214@qq.com>
2020-03-12 00:00:01 +0800

    配置文件修改

---
+++ src/main/java/com/lin/imissyou/model/Theme.java
@@ -0,0 +1,24 @@
+package com.lin.imissyou.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+public class Theme {
+
+    @Id
+    private Long id;
+    private String title;
+    private String description;
+    private String name;
+    private String tplName;
+    private String entranceImg;
+    private String extend;
+    private String internalTopImg;
+    private String titleImg;
+    private Byte online;
+}
